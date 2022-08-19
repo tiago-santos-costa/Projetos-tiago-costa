3c73b28c8158765cd0a3c10b9bfae1ef118341d9
tiago santos costa <tiagocostta@outlook.com>
2022-08-19 12:45:57 -0300

    melhorias gerais

--- projetoQuyz.py
+++ projetoQuyz.py
@@ -1,92 +1,102 @@
-print('\n>>>>>>>>>> BEM VINDO AO QUIZ DO CHAVES <<<<<<<<<<\n')
-usuario=input('Vamos começar? s/n:')
-resposta='S'
+from time import sleep
+print('='*50)
+print(f'\n{"> > > > BEM VINDO AO QUIZ DO CHAVES < < < <":^50}\n')
+print('='*50)
 
-while resposta == 'S':
-    
-    score = 0
+while True:
+    score=0
+    resposta=str(input('Podemos começar? [S/N] ')).upper()
+    if resposta == 'N':
+        break 
 
-    if usuario != 's':
-        quit()
+    print('\nComeçando',end='',flush=True) 
+    sleep(0.5)
+    print('.',end='',flush=True)
+    sleep(0.5)
+    print('.',end='',flush=True)
+    sleep(0.5)
+    print('.',flush=True)
+    sleep(0.5)
 
-    print(  '\nComeçando ...\n')
-    print('Quantos meses de aluguel seu Madruga deve ao Sr. Barriga? \n\n (a) 13 meses \n (b) 10 meses \n (c) 14 meses \n (d) 9 meses \n')
-    usuario1=input('Resposta:')
-
-    if usuario1 == 'c':
-        print('\n              Parabéns!\n')
-        score = score + 1
-
+    print('\n1) Quantos meses de aluguel seu Madruga deve ao Sr. Barriga?\n'.upper(), '(a) 13 meses \n (b) 10 meses \n (c) 14 meses \n (d) 9 meses \n')
+    resposta=str(input('Digite a letra correspondente: ')).upper().strip()
+    if resposta == 'C':
+        score+=1
+        print(f'\n{"PARABÉNS! Alternativa correta.":^40}')
     else:
-        print('\n              Escolha errada :(\n')
+        print(f'\n{":( Resposta errada":^40}')
+    sleep(0.5)
 
-    print('Qual era o verdadeiro nome do Chaves? \n\n (a) Chespirito \n (b) Roberto Gómez Bolaños \n (c) Carlos Villagrán \n (d) Ramón Valdés \n')
-    usuario2=input('Resposta:')
-
-    if usuario2 == 'b':
-        print('\n              Parabéns!\n')
-        score = score + 1
+    print('\n2) Qual é o nome completo de Sr. Barriga?\n'.upper(), '(a) Zenon Cachalote \n (b) Zenon Barriga e Pesado \n (c) Zenon Pesado e Barriga \n (d) Zenon Dom Barriga e Pesado \n')
+    resposta=str(input('Digite a letra correspondente: ')).upper().strip()
+    if resposta == 'B':
+        score+=1
+        print(f'\n{"PARABÉNS! Alternativa correta.":^40}')
     else:
-        print('\n              Escolha errada :(\n')
+        print(f'\n{":( Resposta errada":^40}')   
+    sleep(0.5)
 
-    print('Qual é o nome completo de Sr. Barriga? \n\n (a) Zenon Cachalote \n (b) Zenon Barriga e Pesado \n (c) Zenon Pesado e Barriga \n (d) Zenon Dom Barriga e Pesado \n')
-    usuario2=input('Resposta:')
-
-    if usuario2 == 'b':
-        print('\n              Parabéns!\n')
-        score = score + 1
+    print('\n3) De quem Seu Madruga é pai?\n'.upper(), '(a) paty \n (b) kiko \n (c) Chiquinha \n (d) Pópis \n')
+    resposta=str(input('Digite a letra correspondente: ')).upper().strip()
+    if resposta == 'C':
+        score+=1
+        print(f'\n{"PARABÉNS! Alternativa correta.":^40}')
     else:
-        print('\n              Escolha errada :(\n')
+        print(f'\n{":( Resposta errada":^40}')   
+    sleep(0.5)
 
-    print('De quem Seu Madruga é pai? \n\n (a) paty \n (b) kiko \n (c) Chiquinha \n (d) Pópis \n')
-    usuario2=input('Resposta:')
-
-    if usuario2 == 'c':
-        print('\n              Parabéns!\n')
-        score = score + 1
+    print('\n4) De quem é a frase "prefiro morrer do que perder a vida"?\n'.upper(), '(a) Seu Madruga \n (b) Chiquinha \n (c) Kiko \n (d) Chaves \n')
+    resposta=str(input('Digite a letra correspondente: ')).upper().strip()
+    if resposta == 'D':
+        score+=1
+        print(f'\n{"PARABÉNS! Alternativa correta.":^40}')
     else:
-        print('\n              Escolha errada :(\n')
+        print(f'\n{":( Resposta errada":^40}')   
+    sleep(0.5)
 
-    print('De quem é a frase "prefiro morrer do que perder a vida"? \n\n (a) Seu Madruga \n (b) Chiquinha \n (c) Kiko \n (d) Chaves \n')
-    usuario3=input('Resposta:')
-
-    if usuario3 == 'd':
-        print('\n              Parabéns!\n')
-        score = score + 1
+    print('\n5) Quem interpretou Professor Girafales?\n'.upper(), '(a) Rubén Aguirre \n (b) Ramón Valdez \n (c) Carlos Villagran \n (d) Edgar Vivar \n')
+    resposta=str(input('Digite a letra correspondente: ')).upper().strip()
+    if resposta == 'A':
+        score+=1
+        print(f'\n{"PARABÉNS! Alternativa correta.":^40}')
     else:
-        print('\n              Escolha errada :(\n')
+        print(f'\n{":( Resposta errada":^40}')   
+    sleep(0.5)
 
-    print('Quem interpretou Professor Girafales? \n\n (a) Rubén Aguirre \n (b) Ramón Valdez \n (c) Carlos Villagran \n (d) Edgar Vivar \n')
-    usuario2=input('Resposta:')
-
-    if usuario2 == 'a':
-        print('\n              Parabéns!\n')
-        score = score + 1
+    print('\n6) No episódio do festival da boa vizinhança, quem conta a história do cão arrependido?\n'.upper(), '(a) Chaves \n (b) Kiko \n (c) Chiquinha \n (d) Seu Madruga \n')
+    resposta=str(input('Digite a letra correspondente: ')).upper().strip()
+    if resposta == 'A':
+        score+=1
+        print(f'\n{"PARABÉNS! Alternativa correta.":^40}')
     else:
-        print('\n              Escolha errada :(\n')
-        
-    print('No episódio do festival da boa vizinhança, quem conta a história do cão arrependido? \n\n (a) Chaves \n (b) Kiko \n (c) Chiquinha \n (d) Seu Madruga \n')
-    usuario2=input('Resposta:')
+        print(f'\n{":( Resposta errada":^40}')   
+    sleep(0.5)
 
-    if usuario2 == 'a':
-        print('\n              Parabéns!\n')
-        score = score + 1
+    print('\n7) Quem leva Chaves para Acapulco?\n'.upper(), '(a) Prof. Girafales \n (b) A bruxa do 71 \n (c) Seu Madruga \n (d) Sr. Barriga \n')
+    resposta=str(input('Digite a letra correspondente: ')).upper().strip()
+    if resposta == 'D':
+        score+=1
+        print(f'\n{"PARABÉNS! Alternativa correta.":^40}')
     else:
-        print('\n              Escolha errada :(\n')
+        print(f'\n{":( Resposta errada":^40}')   
+    sleep(0.5)
 
-    print('Quem leva Chaves para Acapulco? \n\n (a) Prof. Girafales \n (b) A bruxa do 71 \n (c) Seu Madruga \n (d) Sr. Barriga \n')
-    usuario2=input('Resposta:')
-
-    if usuario2 == 'd':
-        print('\n           Parabéns!\n')
-        score = score + 1
+    print('\n8) Qual era o verdadeiro nome do Chaves?\n'.upper(), '(a) Chespirito \n (b) Roberto Gómez Bolaños \n (c) Carlos Villagrán \n (d) Ramón Valdés \n')
+    resposta=str(input('Digite a letra correspondente: ')).upper().strip()
+    if resposta == 'B':
+        score+=1
+        print(f'\n{"PARABÉNS! Alternativa correta.":^40}')
     else:
-        print('\n              Escolha errada :(\n')
+        print(f'\n{":( Resposta errada":^40}')   
+    sleep(0.5)
 
     if score == 8:
-        print('Parabén pontuação máxima!')
+        print('\n    Parabén pontuação máxima!'.upper())
+    print(f'  Total de pontos: {score} (de 8 possíveis)  \n\nQuiz acabou :)\n')
 
-    
-    print('  Total de pontos: {} (de 8 possíveis)  \n\nQuiz acabou :)\n'.format(score))
-    resposta=str(input('Jogar novamente? ')).upper().strip()[0]
-print('\n      FIM')
+    resposta=str(input('\nJogar novamente? [S/N] ')).upper().strip()[0]
+    if resposta == 'N':
+        break
+print('='*20)
+print('    VOLTE SEMPRE ')
+print('='*20)
